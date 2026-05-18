435afd058d4450b6d71c09dcc94a005d7c6d3a25
Christoph  Niederbudde <niederch@mail.de>
2026-05-18 13:26:07 +0200

    Balancing factor

--- avl/avl-functional/_build/default/lib/datastructure.ml
+++ avl/avl-functional/_build/default/lib/datastructure.ml
@@ -1,28 +1,35 @@
-module Avl = struct
-  type color = Red | Black
-end
+type bal = LH | BAL | RH
+type 'a avl = Leaf | Node of (bal * 'a * 'a avl * 'a avl)
 
-type 'a avl = Leaf | Node of (int * 'a * 'a avl * 'a avl)
-
-let ht tree = 
+let rec ht tree = 
   match tree with 
   Leaf -> 0 |
-  Node (h, _, _, _) -> h
+  Node (BAL, _, t1, t2) -> ht t1 + 1 |
+  Node (LH, _, t1, t2) -> ht t1 + 1 |
+  Node (RH, _, t1, t2) -> ht t2 + 1
 
 let rec avl_inv tree =
   match tree with
   Leaf -> true |
-  Node (h, _, left, right) -> h = max (ht left) (ht right) + 1 &&
-                              max (ht left) (ht right) <= min (ht left) (ht right) + 1 &&
-                              avl_inv left && avl_inv right
+  Node (LH, _, left, right) -> ht left = ht right + 1 && avl_inv left && avl_inv right |
+  Node (BAL, _, left, right) -> ht left = ht right && avl_inv left && avl_inv right |
+  Node (RH, _, left, right) -> ht left + 1 = ht right && avl_inv left && avl_inv right
+
 
 let rec print_avl_rec tree = match tree with 
   Leaf -> "<>" |
-  Node (h, a, left, right) -> 
-            "(" ^ print_avl_rec left ^ "," ^ string_of_int h ^ "," ^ 
+  Node (BAL, a, left, right) -> 
+            "(" ^ print_avl_rec left ^ ", =, " ^
+            string_of_int a ^ "," ^ print_avl_rec right ^ ")" |
+  Node (LH, a, left, right) -> 
+            "(" ^ print_avl_rec left ^ ", <-, " ^
+            string_of_int a ^ "," ^ print_avl_rec right ^ ")" |
+  Node (RH, a, left, right) -> 
+            "(" ^ print_avl_rec left ^ ", ->, " ^
             string_of_int a ^ "," ^ print_avl_rec right ^ ")"
 
 let print_avl tree = print_newline (print_string (print_avl_rec tree))
+
 
 let rec find x tree =
     match x, tree with
@@ -30,36 +37,78 @@ let rec find x tree =
     a, Node (h, b, left, right) -> a == b || find x left || find x right
 
 
+let is_bal tree = match tree with
+  Leaf -> true |
+  Node(BAL, _, _, _) -> true |
+  tree -> false
+
+let incr old changed = (old == Leaf && changed != Leaf || is_bal old && not (is_bal changed))
+
+let rot2 t1 a t2 c t3 = match t2 with
+  Node (BAL, b, t21, t22) -> Node (BAL, b, Node(BAL, a, t1, t21), Node (BAL, c, t22, t3)) |
+  Node (LH, b, t21, t22) -> Node (BAL, b, Node(BAL, a, t1, t21), Node (RH, c, t22, t3)) |
+  Node (RH, b, t21, t22) -> Node (BAL, b, Node(LH, a, t1, t21), Node (BAL, c, t22, t3)) |
+  (* This case should be impoassible I think *)
+  Leaf -> Leaf 
+
 let balL tree = match tree with
   Leaf -> tree |
-  Node (h, a, Leaf, right) -> tree |
-  Node (h0, c, Node (h1, a, t1, t2), t3) -> 
-    if (h1 = ht t3 + 2) 
-      then if ht t1 >= ht t2 
-        then Node (h0 - 1, a, t1, Node (ht t2 + 1, c, t2, t3)) 
-        else match t2 with Leaf -> tree |
-          Node (h2, b, t21, t22) -> 
-            Node (h0 - 1, b, Node (ht t1 + 1, a, t1, t21), Node (ht t3 + 1, c, t22, t3))
-      else tree
+  Node (b, a, Leaf, right) -> tree |
+  Node (LH, c, Node (RH, a, t1, t2), t3) -> rot2 t1 a t2 c t3 |
+  Node (LH, c, Node (LH, a, t1, t2), t3) -> Node (BAL, a, t1, Node (BAL, c, t2, t3)) |
+  Node (LH, c, Node (BAL, a, t1, t2), t3) -> Node (RH, a, t1, Node (LH, c, t2, t3)) |
+  Node (BAL, c, left, right) -> Node (LH, c, left, right) |
+  Node (RH, c, left, right) -> Node (BAL, c, left, right)
+
 
 let balR tree = match tree with
   Leaf -> tree |
-  Node (h, a, left, Leaf) -> tree |
-  Node (h0, a, t1, Node (h1, c, t2, t3)) ->
-    if (h1 = ht t1 + 2) 
-      then if ht t2 <= ht t3 
-        then Node (h0 - 1, c, Node (ht t2 + 1, a, t1, t2), t3)
-        else match t2 with Leaf -> tree |
-          Node (h2, b, t21, t22) -> 
-            Node (h0 - 1, b, Node (ht t1 + 1, a, t1, t21), Node (ht t3 + 1, c, t22, t3))
-    else tree
+  Node (b, a, left, Leaf) -> tree |
+  Node (RH, a, t1, Node (LH, c, t2, t3)) -> rot2 t1 a t2 c t3 |
+  Node (RH, a, t1, Node (RH, c, t2, t3)) -> Node (BAL, a, Node (BAL, c, t1, t2), t3) |
+  Node (RH, a, t1, Node (BAL, c, t2, t3)) -> Node (RH, a, Node (LH, c, t1, t2), t3) |
+  Node (BAL, a, t1, t2) -> Node (RH, a, t1, t2) |
+  Node (LH, a, t1, t2) -> Node (BAL, a, t1, t2)
+  
+
 
 let rec insert x tree = match tree with
-  Leaf -> Node (1, x, Leaf, Leaf) |
-  Node (h, a, left, right) -> 
+  Leaf -> Node (BAL, x, Leaf, Leaf) |
+  Node (bl, a, left, right) -> 
     if x < a then let new_left = insert x left in 
-      balL (Node (max (ht new_left) (ht right) + 1, a, new_left, right))
-  else if x > a then let new_right = insert x right in 
-      balR (Node (max (ht left) (ht new_right) + 1, a, left, new_right))
+      if incr left new_left 
+        then balL (Node (bl, a, new_left, right))
+        else Node (bl, a, new_left, right)
+    else if x > a then let new_right = insert x right in 
+      if incr right new_right 
+        then balR (Node (bl, a, left, new_right))
+    else Node (bl, a, left, new_right)
   else tree
+
+let decr old changed = match old with 
+  Leaf -> false |
+  old -> incr changed old
+
+let rec split_max tree = match tree with
+  Leaf -> (Leaf, 0) |
+  Node (bl, a, left, Leaf) -> (left, a) |
+  Node (bl, a, left, right) -> 
+    let (r1, a1) = split_max right in 
+    if incr r1 right then (balL (Node (bl, a, left, r1)), a1)
+    else (Node (bl, a, left, r1), a1)
+
+let rec delete x tree = match tree with
+  Leaf -> Leaf |
+  Node (bl, a, left, right) -> 
+    if x < a then let new_left = delete x left in 
+      if decr left new_left then balR (Node (bl, a, new_left, right))
+      else Node (bl, a, new_left, right)
+    else if x > a then let new_right = delete x right in 
+      if decr right new_right then balL (Node (bl, a, left, new_right))
+      else Node (bl, a, left, new_right)
+    else if left == Leaf then right
+    else let (l1, a1) = split_max left in 
+      if incr l1 left 
+        then balR (Node (bl, a1, l1, right))
+        else Node (bl, a1, l1, right)
 
